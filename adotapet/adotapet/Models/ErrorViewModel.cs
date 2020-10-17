1a9d393599067dd2d30a2a2d370c083462ad4559
mikefarias <mikesantosfarias@gmail.com>
2020-10-17 06:31:05 -0300

    Projeto MVC criado no VS

---
+++ adotapet/adotapet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace adotapet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
