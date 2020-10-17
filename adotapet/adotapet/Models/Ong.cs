ee2394c4e3ec443e4d9761418fc964b6495e333e
mikefarias <mikesantosfarias@gmail.com>
2020-10-17 07:57:16 -0300

    Criando models Pet e Ong sem relacionamentos

---
+++ adotapet/adotapet/Models/Ong.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace adotapet.Models
+{
+    public class Ong
+    {
+        public int Id { get; set; }
+
+        public string Name{ get; set; }
+
+        public double Cnpj{ get; set; }
+
+        public string Address { get; set; }
+
+        public string Contact { get; set; }
+
+
+    }
+}
---
+++ adotapet/adotapet/Models/Pet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace adotapet.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string DateOfBirth { get; set; }
+
+        public string Breed { get; set; }
+
+        public Ong Ong { get; set; }
+
+        public double Weight { get; set; }
+
+        public bool IsAdopted { get; set; }
+    }
+}
