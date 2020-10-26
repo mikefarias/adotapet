886653a15dc7012a8ed60698a8d5624f5620da86
mikefarias <mikesantosfarias@gmail.com>
2020-10-26 17:06:32 -0300

    Renomeando apresentação das propriedades

--- adotapet/adotapet/Models/Ong.cs
+++ adotapet/adotapet/Models/Ong.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,12 +11,16 @@ namespace adotapet.Models
     {
         public int Id { get; set; }
 
+        [DisplayName("Nome")]
         public string Name{ get; set; }
 
+        [DisplayName("CNPJ")]
         public double Cnpj{ get; set; }
 
+        [DisplayName("Endereço")]
         public string Address { get; set; }
 
+        [DisplayName("Contato")]
         public string Contact { get; set; }
 
 
--- adotapet/adotapet/Models/Pet.cs
+++ adotapet/adotapet/Models/Pet.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,18 +11,24 @@ namespace adotapet.Models
     {
         public int Id { get; set; }
 
+        [DisplayName("Nome")]
         public string Name { get; set; }
 
+        [DisplayName("Data de Nascimento")]
         public string DateOfBirth { get; set; }
 
+        [DisplayName("Raça")]
         public string Breed { get; set; }
 
+        [DisplayName("Ong")]
         [ForeignKey("Ong")]
         public int IdOng { get; set; }
         public virtual Ong Ong { get; set; }
 
+        [DisplayName("Peso")]
         public double Weight { get; set; }
 
+        [DisplayName("É Adotado? ")]
         public bool IsAdopted { get; set; }
     }
 }
