3f846216bcf91cffb5bc29016b45388aa7e2fae0
mikefarias <mikesantosfarias@gmail.com>
2020-11-16 23:36:08 -0300

    Novo migration para criação de novo banco

--- adotapet/adotapet/Migrations/20201117013517_novoBD.cs
+++ adotapet/adotapet/Migrations/20201117013517_novoBD.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace adotapet.Migrations
 {
-    public partial class novasTabelas2 : Migration
+    public partial class novoBD : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
@@ -24,6 +24,48 @@ namespace adotapet.Migrations
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Adotante", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ong",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Cnpj = table.Column<double>(nullable: false),
+                    Address = table.Column<string>(nullable: true),
+                    Contact = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ong", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pet",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    Abstract = table.Column<string>(nullable: true),
+                    Photo = table.Column<string>(nullable: true),
+                    DateOfBirth = table.Column<string>(nullable: true),
+                    Breed = table.Column<string>(nullable: true),
+                    IdOng = table.Column<int>(nullable: false),
+                    Weight = table.Column<double>(nullable: false),
+                    IsAdopted = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pet", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Pet_Ong_IdOng",
+                        column: x => x.IdOng,
+                        principalTable: "Ong",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
                 });
 
             migrationBuilder.CreateTable(
@@ -64,6 +106,11 @@ namespace adotapet.Migrations
                 name: "IX_Entrevista_PetId",
                 table: "Entrevista",
                 column: "PetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pet_IdOng",
+                table: "Pet",
+                column: "IdOng");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -73,6 +120,12 @@ namespace adotapet.Migrations
 
             migrationBuilder.DropTable(
                 name: "Adotante");
+
+            migrationBuilder.DropTable(
+                name: "Pet");
+
+            migrationBuilder.DropTable(
+                name: "Ong");
         }
     }
 }
