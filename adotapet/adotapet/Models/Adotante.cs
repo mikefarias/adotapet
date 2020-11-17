001c037f00d99a8f7d321b831daeea6a9b11e62b
mikefarias <mikesantosfarias@gmail.com>
2020-11-16 21:44:11 -0300

    Novas models Entrevista e Adotante

---
+++ adotapet/adotapet/Models/Adotante.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace adotapet.Models
+{
+    public class Adotante
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string DataNascimento { get; set; }
+        public string RG{ get; set; }
+
+        public string CPF { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string Profissao { get; set; }
+
+        public string Celular { get; set; }
+    }
+}
---
+++ adotapet/adotapet/Models/Entrevista.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace adotapet.Models
+{
+    public class Entrevista
+    {
+        public int Id { get; set; }
+
+        public int IdPet { get; set; }
+        public virtual Pet Pet { get; set; }
+
+        public int IdAdotante { get; set; }
+
+        public virtual Adotante Adotante { get; set; }
+
+        public DateTime Data { get; set; }
+
+    }
+}
