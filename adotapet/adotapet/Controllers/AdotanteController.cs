40d130fdff5429b977225c1c75f731bb71587ab7
mike <mike@protechsolutions.com.br>
2020-11-18 16:05:41 -0300

    Realizando a sepação dos models para Camada Domain

--- adotapet/adotapet/Controllers/AdotanteController.cs
+++ adotapet/adotapet/Controllers/AdotanteController.cs
@@ -2,10 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using adotapet.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using adotapet.Models;
 
 namespace adotapet.Controllers
 {
