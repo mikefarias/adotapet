eaf1a44307aecb9f7d3ca680ec191bc812e3fde7
mikefarias <mikesantosfarias@gmail.com>
2020-12-21 13:51:45 -0300

    Utilizando LazyLoading para carregar objetos relacionados

--- adotapet/Domain/Entities/Context.cs
+++ adotapet/Domain/Entities/Context.cs
@@ -4,22 +4,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Domain.Entities;
+using Microsoft.EntityFrameworkCore.Proxies;
 
 namespace Domain.Entities
 {
     public class Context : DbContext
     {
-        public DbSet<Pet> Pet { get; set; }
-
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\mssqllocaldb;Database=adotapet3;Integrated Security=True");
+            optionsBuilder.UseLazyLoadingProxies();
         }
 
+        public DbSet<Pet> Pet { get; set; }
         public DbSet<Ong> Ong { get; set; }
-
         public DbSet<Adotante> Adotante { get; set; }
-
         public DbSet<Entrevista> Entrevista { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Pet>()
+                .HasOne(p => p.Ong);
+        }
+
     }
 }
