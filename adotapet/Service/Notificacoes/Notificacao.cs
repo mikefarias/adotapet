33782fb26f2a5905923e4e89f544f3091b044012
4042190 <mike@protechsolutions.com.br>
2021-01-23 10:35:04 -0300

    Criação de notificador para tratamento de erros

---
+++ adotapet/Service/Notificacoes/Notificacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Notificacoes
+{
+    public class Notificacao
+    {
+        public Notificacao(string mensagem)
+        {
+            Mensagem = mensagem;
+        }
+        public string Mensagem { get; }
+    }
+}
---
+++ adotapet/Service/Notificacoes/Notificador.cs
@@ -0,0 +1,19 @@
+﻿using Service.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Service.Notificacoes
+{
+    class Notificador : INotificador
+    {
+        private List<Notificacao> _notificacoes;
+
+        public void Handle(Notificacao notificacao) => _notificacoes = new List<Notificacao>();
+
+        public List<Notificacao> ObterNotificacoes() => _notificacoes;
+
+        public bool TemNotificacao() => _notificacoes.Any();
+    }
+}
